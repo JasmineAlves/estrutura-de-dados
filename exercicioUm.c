1e17bca42ee6fbf42e5dc44342ae65019374a7c2
Jasmine Masson Alves <137460380+JasmineAlves@users.noreply.github.com>
2024-09-01 23:54:50 -0300

    Update exercicioUm.c

--- exercicioUm.c
+++ exercicioUm.c
@@ -1,11 +1,13 @@
-/*Atividade 1
+/* ATIVIDADE 1
 1.1. Escreva uma função em c responsável por imprimir o conteúdo dos n
 elementos de um array de inteiros, conforme protótipo abaixo:
 void printvint(int a[], int n);
+
 1.2. Escreva uma função em c responsável por comparar o conteúdo dos n
 elementos de 2 arrays e retornar 1 se forem iguais ou 0 se forem diferentes,
 conforme protótipo abaixo:
 int cmpvint(int a1[], int a2[], int n);
+
 1.3. Escreva uma função em c responsável por verificar se um caractere c
 existe em uma string e retornar 1 caso exista ou 0 caso contrário, conforme
 protótipo abaixo:
